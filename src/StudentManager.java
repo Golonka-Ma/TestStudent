019c857ff16b188ede4516cdb64a43b4916ec19f
Marcin Golonka <BattelShark@gmail.com>
2024-03-08 10:49:05 +0100

    RGR_4: Implementacja metody addGrade

--- src/StudentManager.java
+++ src/StudentManager.java
@@ -1,11 +1,14 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class StudentManager {
 
     private HashMap<String, String[]> students = new HashMap<>();
+    private HashMap<String, ArrayList<Double>> grades = new HashMap<>();
 
     public boolean addStudent(String name, String surname, String studentId) {
         students.put(studentId, new String[]{name, surname});
+        grades.put(studentId, new ArrayList<>());
         return true;
     }
 
@@ -20,6 +23,15 @@ public class StudentManager {
     public boolean removeStudent(String studentId) {
         if (students.containsKey(studentId)) {
             students.remove(studentId);
+            grades.remove(studentId);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addGrade(String studentId, String subject, double grade) {
+        if (students.containsKey(studentId)) {
+            grades.get(studentId).add(grade);
             return true;
         }
         return false;
--- src/StudentManagerTest.java
+++ src/StudentManagerTest.java
@@ -4,9 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class StudentManagerTest {
 
     @Test
-    void testRemoveStudent() {
+    void testAddGrade() {
         StudentManager studentManager = new StudentManager();
         studentManager.addStudent("John", "Doe", "12345");
-        assertTrue(studentManager.removeStudent("12345"));
+        assertTrue(studentManager.addGrade("12345", "Math", 4.5));
     }
 }
